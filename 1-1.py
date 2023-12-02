3890dd8826f42e8672337cee60251f15cfe5143c
piscythe <malfunctioningadamantium@gmail.com>
2023-12-02 12:29:34 -0800

    days 1-2

---
+++ 1-1.py
@@ -0,0 +1,34 @@
+file1 = open('input1-1.txt', 'r')
+input = file1.readlines()
+
+#this is what happens when you code at 1:30 AM
+numberMap = {
+    "one" : "o1ne",
+    "two" : "t2wo",
+    "three" : "t3hree",
+    "four" : "f4our",
+    "five" : "f5ive",
+    "six" : "s6ix",
+    "seven" : "s7even",
+    "eight" : "e8ight",
+    "nine": "n9ine"
+}
+
+total = 0
+for line in input:
+    for key in numberMap:
+        line = line.replace(key, numberMap[key])
+    print(line)
+    first = 0
+    firstSet = False
+    last = 0
+    for i in range(len(line)):
+        char = line[i]
+        if char.isnumeric():
+            if not firstSet:
+                firstSet = True
+                first = int(char)*10
+            last = int(char)
+    total += first + last
+
+print(total)
---
+++ 2-1.py
@@ -0,0 +1,39 @@
+import re
+
+file1 = open('input2-1.txt', 'r')
+input = file1.readlines()
+
+colorMaxes = {
+    "red": 12,
+    "green": 13,
+    "blue": 14
+}
+
+total = 0
+totalPower = 0
+for line in input:
+    data = re.split("[;:,]", line)
+    index = 0
+    possible = True
+    # this will be fiiiiine
+    colorMins = {
+        "red": 0,
+        "blue": 0,
+        "green": 0
+    }
+    for d in data:
+        tokens = d.split()
+        if tokens[0] == "Game":
+            index = int(tokens[1])
+        if tokens[0].isnumeric():
+            val = int(tokens[0])
+            if val > colorMaxes[tokens[1]]:
+                possible = False
+            if val > colorMins[tokens[1]]:
+                colorMins[tokens[1]] = val
+    if possible:
+        total += index
+    totalPower += colorMins["red"]*colorMins["blue"]*colorMins["green"]
+
+print("sum of ids of possible games " + str(total))
+print("sum of powers " + str(totalPower))
